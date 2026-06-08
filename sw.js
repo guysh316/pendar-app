c1a80e48131d5826c4e02f7b3a24fdfea8d5348b
guysh316 <155192467+guysh316@users.noreply.github.com>
2026-06-08 22:03:13 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "cafe-menu-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/data.js",
+  "/assets/fonts/Peyda-Regular.ttf",
+  "/manifest.json",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
